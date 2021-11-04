458eff87c47cf47103b650484eb56203fe0554bc
Bruno T. Russo <brusso@gmail.com>
2021-11-03 21:30:17 -0300

    ajuste no arquivo main.tf

--- AWS/terraform/s3-site-estatico/main.tf
+++ AWS/terraform/s3-site-estatico/main.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "site_estatico" {
   bucket = var.bucket_name
   acl    = "public-read"
-#  policy = file("policy.json")
   website {
     index_document = "index.html"
     error_document = "error.html"
